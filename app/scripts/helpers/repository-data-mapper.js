f8d297353f33accefde38b9100cb877ca680af49
Will Mendes <willmendesneto@gmail.com>
2016-04-26 22:02:56 +1000

    Add new classes and icons based in cctray response status

--- app/scripts/helpers/repository-data-mapper.js
+++ app/scripts/helpers/repository-data-mapper.js
@@ -3,6 +3,41 @@
 import $ from 'jquery';
 import CONFIG from '../constants/AppConstants';
 
+const getClassByBuildStatus = (lastBuildStatus) => {
+  let statusClassname = '';
+  switch(lastBuildStatus) {
+    case CONFIG.BUILD_STATUS_SUCCESS:
+      statusClassname = CONFIG.CARD_SUCCESS_CLASS;
+      break;
+    case CONFIG.BUILD_STATUS_PENDING:
+      statusClassname = CONFIG.CARD_PENDING_CLASS;
+      break;
+    case CONFIG.BUILD_STATUS_UNKNOWN:
+    statusClassname = CONFIG.CARD_UNKNOWN_CLASS;
+      break;
+    default:
+      statusClassname = CONFIG.CARD_ERROR_CLASS;
+      break;
+  }
+  return statusClassname;
+};
+
+const getBuildIconByBuildStatus = (lastBuildStatus) => {
+  let statusBuildIcon = '';
+  switch(lastBuildStatus) {
+    case CONFIG.BUILD_STATUS_SUCCESS:
+      statusBuildIcon = CONFIG.SUCCESS_ICON;
+      break;
+    case CONFIG.BUILD_STATUS_PENDING:
+      statusBuildIcon = CONFIG.PENDING_ICON;
+      break;
+    default:
+      statusBuildIcon = CONFIG.ERROR_ICON;
+      break;
+  }
+  return statusBuildIcon;
+};
+
 let RepositoryDataMapper = {
   parse: (data) => {
     let xml = $($.parseXML(data));
@@ -15,8 +50,8 @@ let RepositoryDataMapper = {
         lastBuildLabel: el.attr('lastBuildLabel'),
         lastBuildTime: el.attr('lastBuildTime'),
         lastBuildStatus: lastBuildStatus,
-        buildIcon : (lastBuildStatus === CONFIG.BUILD_STATUS_SUCCESS ? CONFIG.SUCCESS_ICON : CONFIG.ERROR_ICON),
-        class: lastBuildStatus === CONFIG.BUILD_STATUS_SUCCESS ? CONFIG.CARD_SUCCESS_CLASS : CONFIG.CARD_ERROR_CLASS
+        buildIcon : getBuildIconByBuildStatus(lastBuildStatus),
+        class: getClassByBuildStatus(lastBuildStatus)
       };
     })[0];
   }
