5a8155a945710e5ef844796547f638e514ab25fc
Will Mendes <willmendesneto@gmail.com>
2016-04-26 21:55:25 +1000

    Add new constants for status and icons

--- app/scripts/constants/AppConstants.js
+++ app/scripts/constants/AppConstants.js
@@ -2,9 +2,16 @@
 
 export default {
   ENV: (process.env.NODE_ENV === 'development' ? 'DEV' : 'PROD'),
+  PENDING_ICON:'•••',
   ERROR_ICON:'✖',
   SUCCESS_ICON:'✔',
   CARD_SUCCESS_CLASS: 'success',
   CARD_ERROR_CLASS: 'error',
-  BUILD_STATUS_SUCCESS: 'Success'
+  CARD_PENDING_CLASS: 'pending',
+  CARD_UNKNOWN_CLASS: 'unknown',
+  BUILD_STATUS_SUCCESS: 'Success',
+  BUILD_STATUS_FAILURE: 'Failure',
+  BUILD_STATUS_PENDING: 'Pending',
+  BUILD_STATUS_EXCEPTION: 'Exception',
+  BUILD_STATUS_UNKNOWN: 'Unknown'
 };
