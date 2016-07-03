aa68a92b28b7c46aa79bd0e77df00308e0eeb822
Will Mendes <willmendesneto@gmail.com>
2016-07-04 08:59:34 +1000

    Change message when app is offline

--- app/scripts/offline/components/offline.jsx
+++ app/scripts/offline/components/offline.jsx
@@ -14,7 +14,7 @@ export default class Offline extends React.Component {
         <h1 className="title">Build Checker App</h1>
         <br />
         <br />
-        <p className="offline-message">Your app is offline</p>
+        <p className="offline-message">Your app is offline. I will update automatically when the internet is back.</p>
         <img className="offline-icon" src="./assets/images/output.svg" />
       </div>
     )
