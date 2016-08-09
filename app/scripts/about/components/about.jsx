682c0e6b1cdc662a9e76802b1b14657e20936070
Will Mendes <willmendesneto@gmail.com>
2016-08-09 14:29:02 +1000

    Bump version from `v0.0.9`

--- app/scripts/about/components/about.jsx
+++ app/scripts/about/components/about.jsx
@@ -14,7 +14,7 @@ export default class About extends React.Component {
         <h1 className="title">Build Checker App</h1>
         <br />
         <br />
-        <p>Version: 0.0.9</p>
+        <p>Version: 0.0.10</p>
       </div>
     )
   }
